909c760f0d63929fe4bb0b9aafc3811562fbaeb6
Adrien Nader <camaradetux@gmail.com>
2009-09-06 20:17:27 +0200

    New "juju" test package.

---
+++ juju/package_script.el
@@ -0,0 +1,21 @@
+(
+ (
+  (credits "me")
+  (package_name "juju")
+  (package_size_expanded "1MB")
+  (package_type Other)
+  (package_version (Beta 0 0 0 0))
+  (packager_email "a@a.com")
+  (packager_name "ME")
+ )
+ (
+  ("prefix" (MKdir "${PREFIX}/prefix"))
+  ("prefix" (MKdir "${PREFIX}/prefix/pouet"))
+  ("t" (MKdir "${PREFIX}/prefix/t"))
+  ("t" (Expand "x/z/omega" "${PREFIX}/prefix/t"))
+ )
+ (
+  (Reverse "t")
+  (Reverse "prefix") 
+ )
+)
--- me.sh
+++ me.sh
@@ -3,6 +3,9 @@ export OCAMLRUNPARAM="b"
 export PREFIX="."
 
 tar cf test.tar -C test_package . &&
+tar cf juju.tar -C juju . &&
 rm -rf prefix db &&
 ocamlbuild yaxmpkg.native -- -install test.tar &&
+ocamlbuild yaxmpkg.native -- -install juju.tar &&
+read
 ./yaxmpkg.native -uninstall test
