fba7bf07b200dc098323167007b8a15f4fa72925
Adrien Nader <camaradetux@gmail.com>
2010-02-05 23:36:33 +0100

    [ makeypkg ] Use the proper package size and name in the metadata.

--- makeypkg/makeypkg.ml
+++ makeypkg/makeypkg.ml
@@ -4,14 +4,14 @@ let meta ~pkg_name ~pkg_size =
   String.concat "\n" [
     "((credits \"me\")";
     sprintf "(package_name \"%s\")" pkg_name;
-    sprintf "(package_size_expanded \"%fMB\")" pkg_size;
+    sprintf "(package_size_expanded \"%s\")" pkg_size;
     "(package_type Other)";
     "(package_version (Beta 0 0 0 0))";
     "(packager_email \"a@a.com\")";
     "(packager_name \"ME\"))";
   ]
 
-let package_script_el f_list =
+let package_script_el f_list ~pkg_size =
   let f_list = List.filter ((<>) "install") f_list in
   let f file =
     sprintf "(\"%s\" (Expand \"%s\" \"%s\"))" file file "."
@@ -19,7 +19,7 @@ let package_script_el f_list =
   let g file =
     sprintf "(Reverse \"%s\")" file
   in
-  let meta = meta ~pkg_name:"juju" ~pkg_size:1. in
+  let meta = meta ~pkg_name:"juju" ~pkg_size in
   let install = String.concat "\n" (List.map f f_list) in
   let uninstall = String.concat "\n" (List.map g f_list) in
   sprintf "(\n%s\n(\n%s\n)\n(\n%s\n)\n)" meta install uninstall
@@ -28,7 +28,9 @@ let () =
   let output = Sys.argv.(1) in
   assert (Filename.check_suffix output ".txz");
   let f_list = Array.to_list (Sys.readdir ".") in
-  let package_script_el = package_script_el f_list in
+  let pkg_size, _ = FileUtil.StrUtil.du [ "." ] in
+  let pkg_size = FileUtil.string_of_size pkg_size in
+  let package_script_el = package_script_el ~pkg_size f_list in
   let oc = open_out_bin "package_script.el" in
   let () = output_string oc package_script_el in
   let () = close_out oc in
