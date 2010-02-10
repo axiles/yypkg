f18c43353dceb2a26dc2fe166b091d1b23bcec55
Adrien Nader <camaradetux@gmail.com>
2010-02-10 01:41:35 +0100

    [ yaxmpkg ] Rework the command-line parsing. Take a -prefix argument rather than a PREFIX env var.

--- src/yaxmpkg.ml
+++ src/yaxmpkg.ml
@@ -1,31 +1,39 @@
 open Types
 open Lib
 
-let parse_args argv =
-  match argv.(1) with
-    | "-add"
-    | "-install" -> Install argv.(2)
-    | "-remove"
-    | "-uninstall" -> Uninstall argv.(2)
-    | "-list" -> List
-    | _ -> assert false
+let install db p () =
+  let p = FilePath.DefaultPath.make_absolute install_path p in
+  assert (Sys.file_exists p);
+  let updated_db = Install.install_package db p in
+  Db.write db_path updated_db
+
+let uninstall db p () =
+  assert (List.exists (fun ((m, _, _), _) -> m.package_name = p) db);
+  let updated_db = Uninstall.uninstall_package db p in
+  Db.write db_path updated_db
+
+let list db _ () =
+  List.iter (function (m, _, _), _ -> print_endline m.package_name) db
 
 let main () =
   let db = Db.read db_path in
-  let prefix = Sys.getenv "PREFIX" in
-  let () = ignore (mkdir prefix) in
-  let () = Sys.chdir prefix in
-  match parse_args Sys.argv with
-    | Install p -> 
-        let p = FilePath.DefaultPath.make_absolute install_path p in
-        assert (Sys.file_exists p);
-        let updated_db = Install.install_package db p in
-        Db.write db_path updated_db
-    | Uninstall p -> 
-        assert (List.exists (fun ((m, _, _), _) -> m.package_name = p) db);
-        let updated_db = Uninstall.uninstall_package db p in
-        Db.write db_path updated_db
-    | List ->
-        List.iter (function (m, _, _), _ -> print_endline m.package_name) db
+  let f = ref (fun () -> ()) in
+  let prefix = ref "" in
+  let set_f g s =
+    f := g db s
+  in
+  let lst = [
+    "-prefix", Arg.Set_string prefix, "prefix for the package management (mandatory)";
+    "-install", Arg.String (set_f install), "install a package";
+    "-uninstall", Arg.String (set_f uninstall), "uninstall a package";
+    "-list", Arg.String (set_f list), "list packages";
+  ]
+  in
+  let usage_msg = "pouet" in
+  let () = Arg.parse lst (fun _ -> ()) usage_msg in
+  assert ("" <> !prefix );
+  let () = ignore (mkdir !prefix) in
+  let () = Sys.chdir !prefix in
+  !f ()
 
 let () = main ()
