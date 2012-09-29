21dd71525b0b8e047e062de83f5ee524994a95d3
Adrien Nader <adrien@notk.org>
2012-09-29 11:09:23 +0200

    sherpa: rename argument -output-folder to -download-folder.

--- src/sherpa/sherpa.ml
+++ src/sherpa/sherpa.ml
@@ -8,23 +8,24 @@ let x_must_be_provided spec x =
 
 let main () =
   let with_deps = ref false in
-  let output_folder = ref Sherpalib.default_output_folder in
+  let download_folder = ref Sherpalib.default_download_folder in
   let package = ref "" in
   let spec = [
     "-with-deps", Arg.Set with_deps, "get the package dependencies too";
-    "-output-folder", Arg.Set_string output_folder, "folder to download packages to";
+    "-download-folder", Arg.Set_string download_folder,
+      "folder to download packages to";
   ]
   in
   Arg.parse spec ((:=) package) usage_msg;
-  if "" = !output_folder then
-    (x_must_be_provided spec "-output-folder"; 1)
+  if "" = !download_folder then
+    (x_must_be_provided spec "-download-folder"; 1)
   else
     if "" = !package then
       (x_must_be_provided spec "package name"; 1)
     else
       let conf = Sherpalib.read () in
       (ignore (Sherpalib.get_packages ~conf ~with_deps:!with_deps
-        ~output_folder:!output_folder ~package:!package); 0)
+        ~download_folder:!download_folder ~package:!package); 0)
 
 let () = 
   exit (main ())
