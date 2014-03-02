d045d11721fff9245e58737b5cc68abe656c75b6
Adrien Nader <adrien@notk.org>
2014-03-02 21:58:17 +0100

    Web_install: export get_packages.

--- src/yypkg/web_install.mli
+++ src/yypkg/web_install.mli
@@ -1,2 +1,5 @@
+val get_packages :
+  conf:Types.conf -> follow:bool -> dest:FilePath.filename
+  -> packages:string list -> string list
 val cli_spec : Args.child
 val main : start_dir:FilePath.filename -> Args.opt list -> unit
